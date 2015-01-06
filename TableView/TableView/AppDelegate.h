3351ced541d2d681657cef788f9da6d90c7c198b
alexchueh <shadow@pixnet.tw>
2015-01-06 09:50:53 +0800

    Create TableView

---
+++ TableView/TableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableView
+//
+//  Created by shadow on 2015/1/6.
+//  Copyright (c) 2015年 shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableView/TableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TableView
+//
+//  Created by shadow on 2015/1/6.
+//  Copyright (c) 2015年 shadow. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
